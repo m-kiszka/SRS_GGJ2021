261d68593c6985a64ff8419e7ed44753b7b9ea84
emkiusz <mkiszka525@gmail.com>
2021-01-31 06:10:02 +0100

    Punkty na mapie

--- Assets/Scripts/displayNameScript.cs
+++ Assets/Scripts/displayNameScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class bookScript : MonoBehaviour
+public class displayNameScript : MonoBehaviour
 {
     public Text bookHoverText;
     private void Start()
@@ -12,16 +12,10 @@ public class bookScript : MonoBehaviour
     }
     private void OnMouseEnter()
     {
-        Debug.Log("test");
         bookHoverText.text = gameObject.name;
     }
     private void OnMouseExit()
     {
         bookHoverText.text = "";
-    }
-
-    private void OnMouseDown()
-    {
-        
     }
 }
